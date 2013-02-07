1aabc5244f04b2136464e2185a117b4f64ca187b
Evan Hahn <me@evanhahn.com>
2013-02-07 02:07:08 -0500

    raindrops falling

--- flores/full.js
+++ flores/full.js
@@ -13,6 +13,13 @@ x = cloud X
 y = cloud Y
 s = cloud size
 
+o = array of objects
+    x: x coordinate
+    y: y coordinate
+    t: type
+       0: raindrop
+       1: flower
+
 d = document
 w = window
 m = Math
@@ -30,19 +37,38 @@ m = Math;
 d.head.appendChild(C);
 
 // Move the cloud you control
-x = y = 0;
-s = 100;
+x = y = s = 100;
 onmousemove = function(E) {
 	x = E.clientX;
 	y = E.clientY;
 };
 
-// The repeated draw function.
-(D = function() {
+// Make the array of objects, which is of a fixed size
+// It starts out with nulls everywhere
+o = [];
+for (I = 1200; I; I --)
+	o.push(null);
+
+// The repeated frame function.
+(F = function() {
 
 	// How big is the canvas?
 	c.width = W = w.innerWidth;
 	c.height = H = w.innerHeight;
+
+	// Drop a raindrop in the first null spot
+	var index  = o.indexOf(null);
+	o[index] = {
+		x: x + (m.random() * s) - (s / 2),
+		y: y - (s / 3),
+		a: 0,
+		t: 0
+	};
+
+	// Objects have a 20-second lifespan
+	setTimeout(function() {
+		o[index] = null;
+	}, 20000);
 
 	with (a) {
 
@@ -50,9 +76,42 @@ onmousemove = function(E) {
 		fillStyle = "#4be";
 		fillRect(0,0,W,H);
 
+		// Deal with the objects
+		fillStyle = "#009";
+		o.forEach(function(obj) {
+
+			// Bail if we've an empty object
+			if (obj == null) {
+				return;
+			}
+
+			// Raindrop
+			else if (!obj.t) {
+
+				// Draw them
+				beginPath();
+				arc(obj.x, obj.y, 5, 0, 7);  // I use 7 because 7 > (2 * pi)
+				fill();
+
+				// Fall!
+				obj.y += 10;
+
+				// If I've hit the bottom, become a beautiful flower
+				if (obj.y > H) {
+					obj.t = 1;
+				}
+
+			}
+
+			// Flower 1
+			else if (obj.t == 1) {
+			}
+
+		});
+
 		// Draw the cloud
 		fillStyle = "#fff";
-		font = (5 * Math.sin(Date.now() / 500) + s) + "px sans-serif";
+		font = (5 * m.sin(Date.now() / 500) + s) + "px serif";
 		textAlign = "center";
 		fillText("☁", x, y);
 		strokeStyle = "#000";
@@ -62,6 +121,6 @@ onmousemove = function(E) {
 	}
 
 	// Repeat!
-	w.requestAnimationFrame(D);
+	w.requestAnimationFrame(F);
 
 })();
